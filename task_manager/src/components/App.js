022d864de03e30a656e40d1da6fcd9a23b009e14
Chigoziri123 <anuna.s@bullseyemanpower.com>
2023-08-18 21:37:30 +0100

    Updated code in App.js, StatusLine.jsand Task.js

--- task_manager/src/components/App.js
+++ task_manager/src/components/App.js
@@ -1,9 +1,70 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.module.css';
+import StatusLine from './StatusLine';
+
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log("using effect")
+  }, []);
+
+  function addEmptyTask(status) {
+    //
+  }
+
+  function addTask(taskToAdd) {
+    //
+  }
+
+  function deleteTask(TaskId) {
+    //
+  }
+
+  function moveTask(id, newStatus) {
+    //
+  }
+
+  function saveTasKsToLocalStorage(Tasks) {
+    //
+  }
+
+  function loadTasksFromLocalStorage() {
+    //
+  }
+
   return (
     <div className="App">
       <h1>Task Manager</h1>
+      <main>
+        <section>
+          <StatusLine 
+          task={tasks}
+          addEmptyTask={addEmptyTask}
+          addTask={addTask}
+          deleteTask={deleteTask}
+          moveTask={moveTask}
+          status="Backlog"
+          />
+          
+          <StatusLine
+          task={tasks}
+          addEmptyTask={addEmptyTask}
+          addTask={addTask}
+          deleteTask={deleteTask}
+          moveTask={moveTask}
+          status="In Progress" />
+          
+          <StatusLine
+          task={tasks}
+          addEmptyTask={addEmptyTask}
+          addTask={addTask}
+          deleteTask={deleteTask}
+          moveTask={moveTask}
+          status="Done" />
+        </section>
+      </main>
     </div>
   );
 }
--- task_manager/src/components/Task.js
+++ task_manager/src/components/Task.js
@@ -1,7 +1,61 @@
 import '../styles/Task.module.css';
+import {useState} from "react"
 
-export default function Task() {
+export default function Task(props) {
+
+  const {addTask, deleteTask, moveTask, task} = props;
+
+  const [urgencyLevel, setUrgencyLevel] = useState(task.urgency);
+  const [collapsed, setCollapsed] = useState(task.isCollapsed);
+  const [formAction, setFormedAction] = useState("");
+
+  function setUrgency(event) {
+    setUrgencyLevel(event.target.attributes.urgency.value)
+  }
+
+  function handleSubmit(event) {
+    ///
+  }
+
+  function handleMoveLeft() {
+
+  }
+
+  function handleMoveRight(){
+
+
+  }
   return (
-    <div>Task</div>
+    <div className={`task $(collapsed ? "collapsedTask" : "")`}>
+      <button onclick={handleMoveLeft} className="button moveTask">&#171;</button>
+      <form onSubmit={handleSubmit} className={collapsed ? "collapsed" : ""}>
+        <input
+          type="text"
+          className="title input"
+          placeholder="Enter Title"
+          disabled={collapsed}
+          defaultTitle={task.title}
+        />
+        <textarea
+        rows="2"
+        className="description input"
+        name="description"
+        placeholder="Enter Description"
+        defaultValue={task.description}
+        />
+        
+        <div className="urgencyLabels">
+          <label className={`low ${urgencyLevel === "low" ? "selected" : ""}`}>
+            <input 
+              urgency="low"
+              onChange={setUrgency}
+              type="radio"
+              name="urgency"
+            />
+          </label>
+        </div>
+      </form> 
+      <button onclick={handleMoveRight} className="button moveTask">&#187;</button>
+    </div>  
   )
 }
